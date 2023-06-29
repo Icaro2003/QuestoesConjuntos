c475d7361b3ac6e9d10fd452a064cbc4fac7d7fe
Ícaro <icarosaragao2003@gmail.com>
2023-06-29 15:01:44 -0300

    alter c file

--- ex003/ex003.c
+++ ex003/ex003.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
+// Função para converter valores lógicos em "V" ou "F"
+char converterValorLogico(int valor) {
+    if (valor)
+        return 'V';
+    else
+        return 'F';
+}
+
+// Emanuel - letra (a)
+
 // Função que implementa a porta NAND
 int portaNAND(int a, int b) {
     // Retorna o resultado da negação da operação lógica AND entre 'a' e 'b'
     return !(a && b);
 }
+
+// Antonio Pedro - letra (b)
 
 // Função que gera a tabela verdade para a porta NAND
 void tabelaNAND() {
@@ -18,17 +30,22 @@ void tabelaNAND() {
     for (a = 0; a <= 1; a++) {
         for (b = 0; b <= 1; b++) {
             resultado = portaNAND(a, b);
-            // Imprime os valores de 'a', 'b' e o resultado da porta NAND
-            printf("%d  %d  |   %d\n", a, b, resultado);
+            
+            // Imprime os valores de 'a', 'b' e o resultado da porta NAND e converte os valores lógicos para "V" ou "F"
+            printf("%c  %c  |   %c\n", converterValorLogico(a), converterValorLogico(b), converterValorLogico(resultado));
         }
     }
 }
+
+// Erik - letra (c)
 
 // Função que implementa a porta OR
 int portaOR(int a, int b) {
     // Retorna o resultado da operação lógica OR entre 'a' e 'b'
     return a || b;
 }
+
+// Ícaro - letra (d)
 
 // Função que gera a tabela verdade para a porta OR
 void tabelaOR() {
@@ -42,8 +59,9 @@ void tabelaOR() {
     for (a = 0; a <= 1; a++) {
         for (b = 0; b <= 1; b++) {
             resultado = portaOR(a, b);
-            // Imprime os valores de 'a', 'b' e o resultado da porta OR
-            printf("%d  %d  |   %d\n", a, b, resultado);
+            
+            // Imprime os valores de 'a', 'b' e o resultado da porta OR e converte os valores lógicos para "V" ou "F"
+            printf("%c  %c  |   %c\n", converterValorLogico(a), converterValorLogico(b), converterValorLogico(resultado));
         }
     }
 }
@@ -53,6 +71,9 @@ int main() {
     tabelaNAND();
     printf("\n");
     tabelaOR();
+
+    printf("Pressione qualquer tecla para sair...");
+    getchar();
 
     return 0;
 }
